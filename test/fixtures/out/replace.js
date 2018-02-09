3404fb97341a458c7a6c4fc431a00118fdb04048
Will Bamford <w.bamford@gmail.com>
2018-02-09 07:00:53 +0000

    More tests

--- test/fixtures/out/replace.js
+++ test/fixtures/out/replace.js
@@ -1,4 +1,11 @@
 module.exports = {
   oFlag_xs: 'ln-o-flag@xs',
-  u1of2_md: 'ln-u-1/2@md'
+  u1of2_md: 'ln-u-1/2@md',
+  cComponent: {
+    element_something: 'ln-c-component__element@something',
+    $modifier_something: 'ln-c-component ln-c-component--modifier@something',
+    element: {
+      $modifier_something: 'ln-c-component__element ln-c-component__element--modifier@something'
+    }
+  }
 }
